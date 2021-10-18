ad46c864486fa09b5bf71e1d81256c6799e4e34d
kobikaradi <kobisagi@gmail.com>
2021-10-19 02:13:20 +0300

    create login function in authController and crate login route

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,6 +11,31 @@ use Illuminate\Http\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+    public function login(Request $request){
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'password'=> 'required|string|confirmed',
+        ]);
+        //Check email
+        $user = User::where('email', $fields['email'])->first();
+       
+
+        //Check password
+        if(!user || !Hash::check($fields['password'],$user->password)){
+            return response([
+                'message'=> 'Bad creds'
+            ],401);
+        }
+        
+        $token = $user->createToken('myapptoken')->plainTextToken;
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+        return response($response,201);
+    }
+
     public function register(Request $request){
         $fields = $request->validate([
             'name' => 'required|string',
